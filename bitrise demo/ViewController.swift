1fbfcd8e93908548b9d8b197a30445117edba1f9
Nguyen Hoang An <an2419998@gmail.com>
2022-06-10 14:44:31 +0700

    add unlabeled

--- bitrise demo/ViewController.swift
+++ bitrise demo/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let label: UILabel = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        styles()
+        layout()
     }
-
-
 }
 
+extension ViewController {
+    func styles() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello world"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16)
+    }
+
+    func layout() {
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
